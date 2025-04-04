4d04ed443106cb86312ad9ded3bdc2a928602ad0
magomedov marif <magomedovmarif2@gmail.com>
2025-04-05 02:33:34 +0300

    fix: message model

--- backend/infrastructure/database/models/message.py
+++ backend/infrastructure/database/models/message.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from backend.infrastructure.database.models.base import Base
 
 
@@ -9,3 +9,4 @@ class Message(Base):
     chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"))
     text: Mapped[str]
     from_user: Mapped[bool]
+    chat = relationship("Chat", back_populates="messages")
