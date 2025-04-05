80a49e671dc35768911c0cd7256df9fc42e42132
magomedov marif <magomedovmarif2@gmail.com>
2025-04-05 18:40:54 +0300

    fix: importf frontend

--- frontend/src/shared/providers/providers.tsx
+++ frontend/src/shared/providers/providers.tsx
@@ -3,7 +3,6 @@
 import React, { FC, PropsWithChildren } from "react";
 import { Provider } from "react-redux";
 import { store } from "../model/store";
-import { ChatCtxProvider } from "@/entities/chat/model/context/chatCtxProvider";
 import ModalProvider from "./modalProvider";
 import { AuthProvider } from "./authProvider";
 import { queryClient } from "../api/queryClient";
@@ -14,13 +13,11 @@ export const Providers: FC<PropsWithChildren> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <ChatCtxProvider>
           <SocketProvider>
             <AuthProvider />
             <ModalProvider />
             {children}
           </SocketProvider>
-        </ChatCtxProvider>
       </Provider>
     </QueryClientProvider>
   );
